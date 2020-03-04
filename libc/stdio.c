ab67925cefe40410d3b99b8b18dac4a734e2d360
Debankle <oliver.debankle@gmail.com>
2020-03-04 22:27:28 +1000

    improved printf but still broken

--- libc/stdio.c
+++ libc/stdio.c
@@ -1,4 +1,26 @@
 #include "stdio.h"
+#include "string.h"
+
+void printf(void *s) {
+    char *printString;
+    char buffer[33];
+
+    switch (_Generic(s, char*: 1, int: 2, bool: 3, default: 0)) {
+        case 1:
+            printString = s;
+            break;
+        case 2:
+            printString = itoa((int)&s, buffer);
+            break;
+        case 0:
+            printString = s;
+            break;
+        default:
+            printString = s;
+    }
+
+    kprint(printString);
+}
 
 uint8_t strlen(const char *s) {
     int l;
--- libc/stdio.h
+++ libc/stdio.h
@@ -2,7 +2,9 @@
 #define OS_STD
 
 #include "stdint.h"
+#include "../drivers/display.h"
 
+void printf(void *s);
 uint8_t strlen(const char *s);
 
 #endif // OS_STD
