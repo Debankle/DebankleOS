85d4933c1578a328ea0e4cd101088a4d61003edc
Debankle <oliver.debankle@gmail.com>
2020-03-04 22:27:11 +1000

    added bool type

--- libc/stddef.h
+++ libc/stddef.h
@@ -1,8 +1,9 @@
-#ifndef OS_STDDEF_h
+#ifndef OS_STDDEF_H
 #define OS_STDDEF_H
 
 #define NULL ((void *)0)
 
 typedef unsigned int size_t;
+typedef enum { false, true } bool;
 
 #endif // OS_STDDEF_H
