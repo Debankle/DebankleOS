da2bc29d3502051b9e04c9fbc8c5767f633e959c
Debankle <oliver.debankle@gmail.com>
2020-03-05 20:19:42 +1000

    working printf with shity hack that failed

--- libc/stdio.h
+++ libc/stdio.h
@@ -2,9 +2,11 @@
 #define OS_STD
 
 #include "stdint.h"
+#include "stddef.h"
 #include "../drivers/display.h"
 
-void printf(void *s);
+// void printf(char *s, ...);
+void printf(void *s, type_t t);
 uint8_t strlen(const char *s);
 
 #endif // OS_STD
